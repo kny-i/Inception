8bd96285371418fc2e79f27f292f935bb41956ca
kny-i <kny_i@keio.jp>
2022-11-23 11:35:36 +0900

    wip

---
+++ yuki/srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mysql_install_db --user=mysql
+mysqld -u mysql --bootstrap << EOF
+	FLUSH PRIVILEGES;
+	CREATE DATABASE ${MYSQL_DATABASE};
+	CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	SET PASSWORD FOR root@localhost=PASSWORD('${MYSQL_ROOT_PASSWORD}');
+	FLUSH PRIVILEGES;
+EOF
+
+mysqld_safe
