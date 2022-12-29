956aad4e61ce912a128684aca756b121708f59fe
kny-i <kny_i@keio.jp>
2022-12-30 05:57:13 +0900

    wip

--- yuki/srcs/requirements/mariadb/tools/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/MYSQL_DATABASE/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_USER/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_PASSWORD/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_ROOT_PASSWORD/root_pw/g' create_db.sql
