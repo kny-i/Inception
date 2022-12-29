8e9e72612d6b300b75a87c790128d05b66e97726
kny-i <kny_i@keio.jp>
2022-12-30 05:46:49 +0900

    動いた　環境変数問題も解決

--- mac_ver/42-inception/srcs/requirements/mariadb/tools/env.sh
+++ mac_ver/42-inception/srcs/requirements/mariadb/tools/env.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sed -i 's/MYSQL_DATABASE/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_USER/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_PASSWORD/wordpress/g' create_db.sql &&  sed -i 's/MYSQL_ROOT_PASSWORD/root_pw/g' create_db.sql
+export MYSQL_ROOT_PASSWORD=root_pw
+export MYSQL_USER=wordpress
+export MYSQL_PASSWORD=wordpress
+export MYSQL_DATABASE=wordpress
+
+sed -i "s/MYSQL_DATABASE/wordpress/g" create_db.sql &&  sed -i "s/MYSQL_USER/wordpress/g" create_db.sql &&  sed -i "s/MYSQL_PASSWORD/wordpress/g" create_db.sql &&  sed -i "s/MYSQL_ROOT_PASSWORD/root_pw/g" create_db.sql
